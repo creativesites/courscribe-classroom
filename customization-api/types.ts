55f72183462ebd7287642e9b52215507a7a84170
Winston <46081328+creativesites@users.noreply.github.com>
2024-05-23 20:46:49 +0200

    init

---
+++ customization-api/types.ts
@@ -0,0 +1,27 @@
+//audio/video toggle state
+export {
+  ToggleState,
+  ClientRoleType,
+} from '../agora-rn-uikit/src/Contexts/PropsContext';
+export type {
+  ContentInterface,
+  ContentStateInterface,
+  ExtenedContentInterface,
+  UidType,
+} from '../agora-rn-uikit';
+export {
+  I18nDeviceStatus,
+  I18nMuteType,
+} from '../src/language/default-labels/videoCallScreenLabels';
+export type {
+  NetworkQualities,
+  I18nRequestConfirmation,
+  I18nMuteConfirmation,
+  deviceDetectionToastSubHeadingDataInterface,
+  sttSpokenLanguageToastHeadingDataType,
+  whiteboardFileUploadToastDataType,
+  publicChatToastSubHeadingDataInterface,
+  privateChatToastHeadingDataInterface,
+  publicAndPrivateChatToastSubHeadingDataInterface,
+} from '../src/language/default-labels/videoCallScreenLabels';
+export type {TextDataInterface} from '../src/language/default-labels';
